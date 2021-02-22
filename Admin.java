6cc2242278a21c0b72f5374fc3ca5245d92f4f59
Rene P Lisasi <renelisasi@hotmail.com>
2021-02-22 18:33:58 -0500

    commit1
    This is the business object for logging in to admin.

---
+++ Admin.java
@@ -0,0 +1,199 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessObjects;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author renel
+ * Admin business object for admin login
+ * THS System Project
+ */
+public class Admin {
+    
+    //Properties
+    String adminId, adminPw;
+    String adminFname, adminLname, adminAddress, adminEmail;
+    
+    //Constructor that passes no arguments
+    public Admin(){
+        adminId = adminPw = null;
+        adminFname = adminLname= adminAddress = adminEmail = null;
+    }
+    
+    //Constructor that passes all arguments
+    public Admin(String id, String pw, String fName, String lName, String address, String email){
+        adminId = id;
+        adminPw = pw;
+        adminFname = fName;
+        adminLname = lName;
+        adminAddress = address;
+        adminEmail = email;
+    }
+    
+            public static void main(String [] args){
+            //Test InstAnciating a Admin object
+            Admin c1 = new Admin();
+            c1.selectDB("e0011");
+            //c2.insertDB(300, 963, "Anna", "Williams", "10 Hems Mansion, Dublin", "awilliams@gcorp.co.ire");
+            //c2.deleteDB();
+            //display
+            c1.displayAdmin();
+            //int pwdb = c2.getAdminPw();
+            //System.out.println("Password for user:" + pwdb );
+        }
+            
+            public void getAccounts(){
+            
+            try {//Load DB Driver
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            Connection c1 = DriverManager.getConnection("jdbc:ucanaccess://"
+                    + "B:\\OneDrive - Chattahoochee Technical College\\SPRING21\\SYSTEMS PROJECT\\Databases\\db.accdb");
+
+      System.out.println("DatAbase connected"); 
+            //Create StAtement
+            java.sql.Statement stmt = c1.createStatement();
+            //update stAtement definition
+            String sql = "select acctNo from Accounts where cid='"+getAdminId()+"'";
+            System.out.println(sql);
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            
+            //Process ResultSet
+            int an;
+            while (rs.next()){
+                an = rs.getInt(1);
+            }
+            c1.close();
+        } catch (Exception se) {
+            System.out.println(se);
+        }
+            } 
+
+        //display method
+        public void displayAdmin(){
+            System.out.println("ID: " + adminId+ " PW: "+ adminPw +" First Name: " + adminFname 
+                    + " Last Name " + adminLname + " Address: " + adminAddress + 
+                    " Email: " + adminEmail);
+            
+        }
+        
+    public void selectDB(String i) {
+        adminId = i;
+        try {    //Load DB Driver
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            Connection c1 = DriverManager.getConnection("jdbc:ucanaccess://"
+                    + "B:\\OneDrive - ChattAhoochee Technical College\\SPRING21\\JAVA 3\\DB\\ChattBankMDB.mdb");
+
+            //Execute SQL StAtement
+            Statement stmt = c1.createStatement();
+            ResultSet rs = stmt.executeQuery("Select * from Admin where adminID = " + i);
+
+            //Process ResultSet
+            rs.next();
+            adminId = rs.getString(1);
+            adminPw = rs.getString(2);
+            adminFname = rs.getString(3);
+            adminLname = rs.getString(4);
+            adminAddress = rs.getString(5);
+            adminEmail = rs.getString(6);
+            getAccounts();
+            c1.close();
+        } catch (Exception se) {
+            System.out.println(se);
+        }
+    } //end selectDB()
+    
+    //insertDB
+        public void insertDB(String id, String pw, String fn, String ln, String adr, 
+                String eml){
+        //assign values
+        adminId = id;
+        adminPw = pw;
+        adminFname = fn;
+        adminLname = ln;
+        adminAddress = adr;
+        adminEmail = eml;
+        
+        try{
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            Connection c1 = DriverManager.getConnection("jdbc:ucanaccess://"
+                    + "B:\\OneDrive - Chattahoochee Technical College\\SPRING21\\SYSTEMS PROJECT\\Databases\\db.accdb");
+
+            System.out.println("DatAbase connected"); 
+            //Create StAtement
+            java.sql.Statement stmt = c1.createStatement();
+            //update stAtement definition
+            String sql2 = "insert into Admin values('"+getAdminId()+"',"+
+                                                        "'"+getAdminPw()+"',"+
+                                                      "'"+getAdminFname()+"',"+ 
+                                                      "'"+getAdminLname()+"',"+ 
+                                                      "'"+getAdminAddress()+"',"+
+                                                        "'"+getAdminEmail()+"'"+
+                                                        ")"; 
+            //print the stAtement
+            System.out.println(sql2);
+            //check for duplicates
+            int n1 = stmt.executeUpdate(sql2);
+            if (n1==1)
+                System.out.println("INSERT Successful!!!");
+            else
+                System.out.println("INSERT FAILED***********");
+            c1.close();
+        }
+        catch(Exception e1){
+            System.out.println(e1);
+        }
+    }
+        //DeleteDB
+        public void deleteDB(){
+                        try{
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+           java.sql.Connection con3 = DriverManager.getConnection("jdbc:ucanaccess://"
+                   + "B:\\OneDrive - Chattahoochee Technical College\\SPRING21\\SYSTEMS PROJECT\\Databases\\db.accdb");
+            System.out.println("DatAbase connected");
+            //Create StAtement - Step #3
+            java.sql.Statement stmt = con3.createStatement();
+
+            //Execute StAtement - Step #4
+            String sql3 = "Delete from Admin where adminId ='"+getAdminId()+"'"; 
+            //Print sql stAtement
+            System.out.println(sql3);
+            //check for duplicates
+            int n1 = stmt.executeUpdate(sql3);
+            if (n1==1)
+                System.out.println("DELETE Successful!!!");
+            else
+                System.out.println("DELETE FAILED***********");
+            con3.close();
+        }
+        catch(Exception e1){
+            System.out.println(e1);
+        }
+        
+        }
+        
+        //getters
+        public String getAdminId(){return adminId;}
+        public String getAdminPw(){return adminPw;}
+        public String getAdminFname() {return adminFname;}
+        public String getAdminLname() {return adminLname;}
+        public String getAdminAddress() {return adminAddress;}
+        public String getAdminEmail() {return adminEmail;}
+            
+        //Setters
+        public void setAdminId (String adminId) {this.adminId = adminId;}
+        public void setAdminPw (String adminPw) {this.adminPw = adminPw;}
+        public void setAdminFname(String adminFname) {this.adminFname = adminFname;}
+        public void setAdminLname(String adminLname) {this.adminLname = adminLname;}
+        public void setAdminAddress(String adminAddress) {this.adminAddress = adminAddress;}
+        public void setAdminEmail(String adminEmail) {this.adminEmail = adminEmail;}
+    }
+

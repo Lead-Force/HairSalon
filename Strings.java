70f729e6035e2baed5e089c10c9f36cfd06e4806
Jean Zambrano <jeanczambrano@hotmail.com>
2021-04-26 13:49:26 -0300

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessObjects;
+
+/**
+ *
+ * @author Jean
+ */
+public class Strings {
+    
+    public static String driver = "net.ucanaccess.jdbc.UcanaccessDriver"; 
+    public static String url = "jdbc:ucanaccess://C:\\Users\\Jean\\Documents\\Systems Project\\Databases\\db.accdb";
+    
+    
+}
